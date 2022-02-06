4f35b83757b1712e268a1e5d2bf120dd72b465b1
Éder Diego de Sousa <me@diego.dev.br>
2022-02-05 21:11:12 -0300

    UserFeignClient

---
+++ hr-oauth/src/main/java/br/dev/diego/hroauth/services/UserService.java
@@ -0,0 +1,28 @@
+package br.dev.diego.hroauth.services;
+
+import br.dev.diego.hroauth.entities.User;
+import br.dev.diego.hroauth.feignclients.UserFeignClient;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    private static Logger logger = LoggerFactory.getLogger(UserService.class);
+
+    @Autowired
+    private UserFeignClient userFeignClient;
+
+    public User findByEmail(String email) {
+        User user = userFeignClient.findByEmail(email).getBody();
+        if(user == null) {
+            logger.error("Email not found " + email);
+            throw new IllegalArgumentException("E-mail not found");
+        }
+        logger.info("Email encontrado: " + email);
+        return user;
+    }
+
+}
